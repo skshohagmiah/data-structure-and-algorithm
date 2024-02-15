1ebf4f4a5e387adfa75abcafb4ac91105da6139a
shohag <shohagmiah2100@gmail.com>
2024-02-15 21:59:39 +0600

    sorting algorithm covered

--- algorithm.js
+++ algorithm.js
@@ -60,3 +60,81 @@
 // }
 
 // console.log(insertionSort([3,2,5,1,6,4]))
+
+
+// quick sort algorithm
+
+// function quickSort(arr){
+//     console.log('inside function call')
+//     if(arr.length <= 1){
+//         return arr
+//     }
+
+//     let pivot = arr[0];
+//     let left = [];
+//     let right = [];
+
+//     for(let i = 1; i<arr.length; i++){
+//         console.log('inside the loop', i)
+//         if(arr[i] > pivot){
+//             right.push(arr[i])
+//         }else{
+//             left.push(arr[i])
+//         }
+//     }
+
+//     return [...quickSort(left), pivot, ...quickSort(right)]
+
+// }
+
+// console.log(quickSort([4,3,2,5,1,7,6]))
+
+
+// merge sort algorithm
+
+function mergeSort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+
+    let mid = Math.floor(arr.length / 2);
+    let leftSlice = arr.slice(0, mid);
+    let rightSlice = arr.slice(mid);
+
+    let sortedLeft = mergeSort(leftSlice);
+    let sortedRight = mergeSort(rightSlice);
+
+    return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right){
+    mergeIndex++
+    let result = []
+    let leftIndex = 0;
+    let rightIndex = 0;
+    
+
+    while(leftIndex < left.length && rightIndex < right.length ){
+        if(left[leftIndex] < right[rightIndex]){
+            result.push(left[leftIndex])
+            leftIndex++
+        }else{
+            result.push(right[rightIndex])
+            rightIndex++
+        }
+    }
+
+    while( leftIndex < left.length){
+        result.push(left[leftIndex])
+        leftIndex++
+    }
+
+    while(rightIndex < right.length){
+        result.push(right[rightIndex])
+        rightIndex++
+    }
+
+    return result
+}
+
+console.log(mergeSort([3,2,5,1,5,6,8,7]))
